fc218749d74b42f33aac9fd2dd5de76f4f16a5e1
Bhagirathi <bhagirathi.s.g05@gmail.com>
2024-01-04 21:50:00 +0530

    my first Commit on jan 4th

---
+++ src/Loops2.java
@@ -0,0 +1,14 @@
+package src;
+
+import java.util.Arrays;
+
+public class Loops2 {
+    public static void main(String[]args){
+        String Name = "Bhagirathi";
+        System.out.println("My name is " +Name);
+        System.out.println('A'==65);
+
+
+    }
+}
+
